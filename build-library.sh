ab40760447bad68adcb351d55b2d674b2c13e9c1
Vit Kucera <vit.kucera@cern.ch>
2019-10-19 21:30:01 +0200

    Clean silently.

--- build-library.sh
+++ build-library.sh
@@ -4,7 +4,7 @@ macroname="testload.C"
 
 echo "ROOT: $ROOT_RELEASE, AliRoot: $ALIROOT_RELEASE, AliPhysics: $ALIPHYSICS_RELEASE"
 echo "Compiling the library"
-rm -r CMakeCache.txt CMakeFiles cmake_install.cmake G__*
+rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake G__*
 cmake . && make
 root -l -b -q $macroname
 
