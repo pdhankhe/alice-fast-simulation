ccd0249ca694007aea98d64513ce4b69d0d5a6fe
Vit Kucera <vit.kucera@cern.ch>
2020-02-28 02:00:58 +0100

    Try to load the library only if make was successful.

--- build-library.sh
+++ build-library.sh
@@ -5,8 +5,7 @@ macroname="testload.C"
 echo "ROOT: $ROOT_RELEASE, AliRoot: $ALIROOT_RELEASE, AliPhysics: $ALIPHYSICS_RELEASE"
 echo "Compiling the library"
 rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake G__*
-cmake . && make
-root -l -b -q $macroname
+cmake . && make && root -l -b -q $macroname
 
 exit 0
 
