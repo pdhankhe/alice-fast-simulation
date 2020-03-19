a18822af35b645a53a95adc3ae5dc9acbfa1fca4
Vit Kucera <vit.kucera@cern.ch>
2020-03-19 20:27:11 +0100

    Delete the library before compilation.

--- build-library.sh
+++ build-library.sh
@@ -10,7 +10,7 @@ fi
 
 echo "ROOT: $ROOT_RELEASE, AliRoot: $ALIROOT_RELEASE, AliPhysics: $ALIPHYSICS_RELEASE"
 echo "Compiling the library"
-rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake G__*
+rm -rf libAnalysisCode.so CMakeCache.txt CMakeFiles cmake_install.cmake G__*
 cmake . && make && root -l -b -q $macroname
 
 exit 0
