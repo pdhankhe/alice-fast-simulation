1f5e380f423ae12a97122686abcce4ead212940d
Vit Kucera <vit.kucera@cern.ch>
2020-04-01 01:34:05 +0200

    Add submit scripts for groups of variations.

---
+++ submit_powheg_pr.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+list=( \
+POWHEG_PYTHIA6_CHARM_13TeV_central \
+POWHEG_PYTHIA6_CHARM_13TeV_F1-R05 \
+POWHEG_PYTHIA6_CHARM_13TeV_F05-R1 \
+POWHEG_PYTHIA6_CHARM_13TeV_F2-R1 \
+POWHEG_PYTHIA6_CHARM_13TeV_F1-R2 \
+POWHEG_PYTHIA6_CHARM_13TeV_F2-R2 \
+POWHEG_PYTHIA6_CHARM_13TeV_F05-R05 \
+POWHEG_PYTHIA6_CHARM_13TeV_Mhi \
+POWHEG_PYTHIA6_CHARM_13TeV_Mlo \
+)
+
+for var in ${list[@]}; do
+    echo -e "Submit \e[1;32m$var\e[0m?"
+    #read -p "Press [ENTER] to continue..." -s
+    #./submit_grid.py ${var}.yaml
+    while true; do
+        read yn
+        case $yn in
+            [Yy] ) echo "Submitting"; ./submit_grid.py ${var}.yaml; break;;
+            [Nn] ) echo "Skipping"; break;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+done
+
+exit 0
+
---
+++ submit_pythia8_pr.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+list=( \
+PYTHIA8_CHARM_13TeV \
+PYTHIA8_CHARM_ColorSoft_13TeV \
+PYTHIA8_CHARM_ColorHard_13TeV \
+)
+
+for var in ${list[@]}; do
+    echo -e "Submit \e[1;32m$var\e[0m?"
+    #read -p "Press [ENTER] to continue..." -s
+    #./submit_grid.py ${var}.yaml
+    while true; do
+        read -p "Answer: " yn
+        case $yn in
+            [Yy] ) echo "Submitting"; ./submit_grid.py ${var}.yaml; break;;
+            [Nn] ) echo "Skipping"; break;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+done
+
+exit 0
+
