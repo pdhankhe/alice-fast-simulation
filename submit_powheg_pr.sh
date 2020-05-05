52011b08382960bcc3b74d2a85e18a73d99e81ff
Vit Kucera <vit.kucera@cern.ch>
2020-05-05 15:26:55 +0200

    Submit all jobs and save stdout, stderr to log files.

--- submit_powheg_pr.sh
+++ submit_powheg_pr.sh
@@ -12,18 +12,25 @@ POWHEG_PYTHIA6_CHARM_13TeV_Mhi \
 POWHEG_PYTHIA6_CHARM_13TeV_Mlo \
 )
 
+#for var in ${list[@]}; do
+#    echo -e "Submit \e[1;32m$var\e[0m?"
+#    #read -p "Press [ENTER] to continue..." -s
+#    #./submit_grid.py ${var}.yaml
+#    while true; do
+#        read -p "Answer: " yn
+#        case $yn in
+#            [Yy] ) echo "Submitting"; ./submit_grid.py ${var}.yaml; break;;
+#            [Nn] ) echo "Skipping"; break;;
+#            * ) echo "Please answer y or n.";;
+#        esac
+#    done
+#done
+
 for var in ${list[@]}; do
-    echo -e "Submit \e[1;32m$var\e[0m?"
-    #read -p "Press [ENTER] to continue..." -s
-    #./submit_grid.py ${var}.yaml
-    while true; do
-        read -p "Answer: " yn
-        case $yn in
-            [Yy] ) echo "Submitting"; ./submit_grid.py ${var}.yaml; break;;
-            [Nn] ) echo "Skipping"; break;;
-            * ) echo "Please answer y or n.";;
-        esac
-    done
+    echo -e "Submitting \e[1;32m$var\e[0m"
+    logfile="${var}.log"
+    ./submit_grid.py ${var}.yaml > $logfile 2>&1
+    sleep 5
 done
 
 exit 0
--- submit_pythia8_pr.sh
+++ submit_pythia8_pr.sh
@@ -2,22 +2,29 @@
 
 list=( \
 PYTHIA8_CHARM_13TeV \
-PYTHIA8_CHARM_ColorSoft_13TeV \
-PYTHIA8_CHARM_ColorHard_13TeV \
+#PYTHIA8_CHARM_ColorSoft_13TeV \
+#PYTHIA8_CHARM_ColorHard_13TeV \
 )
 
+#for var in ${list[@]}; do
+#    echo -e "Submit \e[1;32m$var\e[0m?"
+#    #read -p "Press [ENTER] to continue..." -s
+#    #./submit_grid.py ${var}.yaml
+#    while true; do
+#        read -p "Answer: " yn
+#        case $yn in
+#            [Yy] ) echo "Submitting"; ./submit_grid.py ${var}.yaml; break;;
+#            [Nn] ) echo "Skipping"; break;;
+#            * ) echo "Please answer y or n.";;
+#        esac
+#    done
+#done
+
 for var in ${list[@]}; do
-    echo -e "Submit \e[1;32m$var\e[0m?"
-    #read -p "Press [ENTER] to continue..." -s
-    #./submit_grid.py ${var}.yaml
-    while true; do
-        read -p "Answer: " yn
-        case $yn in
-            [Yy] ) echo "Submitting"; ./submit_grid.py ${var}.yaml; break;;
-            [Nn] ) echo "Skipping"; break;;
-            * ) echo "Please answer y or n.";;
-        esac
-    done
+    echo -e "Submitting \e[1;32m$var\e[0m"
+    logfile="${var}.log"
+    ./submit_grid.py ${var}.yaml > $logfile 2>&1
+    sleep 5
 done
 
 exit 0
