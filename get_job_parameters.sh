abb28da23a9c9892a331d263ba4a2ccd70a49d2a
Vit Kucera <vit.kucera@cern.ch>
2020-05-05 18:40:44 +0200

    Remove special characters.

--- get_job_parameters.sh
+++ get_job_parameters.sh
@@ -2,13 +2,15 @@
 
 # Get the job name and the job ID from the log file.
 
+# removing special characters: https://superuser.com/questions/380772/removing-ansi-color-codes-from-text-stream
+
 list_jobs="jobs.txt"
 
 for var in $(cat $list_jobs); do
     logfile="${var}.log"
     jobname=$(grep "Submitting" $logfile | cut -d " " -f 6)
-    jobid=$(grep "Your new job ID" $logfile | cut -d " " -f 7 | cut -d m -f 3)
-    echo -e "${var}\t${jobname}\t${jobid}\e[0m"
+    jobid=$(grep "Your new job ID" $logfile | cut -d " " -f 7 | sed 's/\x1b\[[0-9;]*m//g')
+    echo -e "${var}\t${jobname}\t${jobid}"
 done
 
 exit 0
